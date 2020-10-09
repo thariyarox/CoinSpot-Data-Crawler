2dbde80a16be1d076697727a19b53084e990d659
Loges R <loges.r@thoughtworks.com>
2020-10-09 14:23:15 +0530

    Fixed Type error and updated relative file path

--- crawler/run.py
+++ crawler/run.py
@@ -1,3 +1,12 @@
 import subprocess
-result = subprocess.Popen('scrapy crawl coinspot -o test_data.csv',
+import os
+
+dirname = os.path.dirname(__file__)
+result = subprocess.Popen('scrapy crawl coinspot -o '+os.path.join(dirname,'../data/test_data.csv'),
                           stdout=subprocess.PIPE, shell=True).communicate()[0]
+data_process = subprocess.Popen('python '+os.path.join(dirname,'../processors/process_data.py'),
+        stdout=subprocess.PIPE, shell=True).communicate()[0]
+print('process start')
+csv_process = subprocess.Popen('python '+os.path.join(dirname,'../processors/process_csv.py'),
+        stdout=subprocess.PIPE, shell=True).communicate()[0]
+print('process end')
